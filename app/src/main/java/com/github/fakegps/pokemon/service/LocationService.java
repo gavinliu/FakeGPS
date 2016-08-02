302a8704cccd0c542583b25fef1ff2ef44f25037
刘云龙 <gavin6liu@gmail.com>
2016-08-03 00:22:04 +0800

    新增通知栏图标

--- app/src/main/java/com/github/fakegps/pokemon/service/LocationService.java
+++ app/src/main/java/com/github/fakegps/pokemon/service/LocationService.java
@@ -3,6 +3,7 @@ package com.github.fakegps.pokemon.service;
 import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v7.app.NotificationCompat;
@@ -24,11 +25,12 @@ public class LocationService extends Service {
 
     private void startNotification() {
         Notification notification = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.icon_app)  // the status icon
-                .setTicker("setTicker")  // the status text
+                .setSmallIcon(R.drawable.ic_notification)  // the status icon
+//                .setTicker("setTicker")  // the status text
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.icon_app))
                 .setWhen(System.currentTimeMillis())  // the time stamp
                 .setContentTitle(getText(R.string.app_name))  // the label
-                .setContentText("12")  // the contents of the entry
+                .setContentText("Working")  // the contents of the entry
 //                .setContentIntent(contentIntent)  // The intent to send when clicked
                 .build();
 
